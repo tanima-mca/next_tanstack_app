1bfd435c5f03fbd139b2aaad9c51afbb2ec79c9d
Tanima Bhattacharyya <tanimateach1982@gmail.com>
2025-03-24 14:49:38 +0530

    updated

--- typeScript/auth.interface.ts
+++ typeScript/auth.interface.ts
@@ -1,11 +1,26 @@
-export interface IloginProps {
-    email: string,
-    password: string,
-    token: string,
-    message: string,
+// export interface IloginProps {
+//     email: string,
+//     password: string,
+//     token: string,
+//     message: string,
+//     status: number;
+//     data:object ;
+//     profile_pic: string;
+// }
+export interface IUserData {
+    profile_pic: string; // Define profile_pic inside a user data object
+    [key: string]: any;  // Allow other dynamic properties
+  }
+  
+  export interface IloginProps {
+    email: string;
+    password: string;
+    token: string;
+    message: string;
     status: number;
-    data:object;
-}
+    data: IUserData;  // Make data follow the IUserData structure
+  }
+  
 export interface IregisterProps {
     first_name: string,
     last_name:string,
